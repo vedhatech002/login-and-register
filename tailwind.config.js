4734ed7c988fac7d8836899de83938c1ee2cfe9b
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-01-17 22:56:31 +0530

    design: complete UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./login.html",
+    "./register.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: " 'Inter', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
